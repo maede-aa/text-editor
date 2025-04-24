1e4917a114121cab024cce8e51ce95c53c3ed36f
maede <maede.aa99@gmail.com>
2025-04-24 22:54:08 +0330

    Implement open, save, and new file functionality in FileManager.

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,53 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            try {
+                Scanner scn = new Scanner(file);
+                StringBuilder content = new StringBuilder();
+                while(scn.hasNextLine()) {
+                    content.append(scn.nextLine()).append("\n");
+                }
+                scn.close();
+                textArea.setText(content.toString());
+                textEditor.currentFile = file;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(textEditor ,"Error reading file!");
+            }
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
-            JFileChooser fileChooser = new JFileChooser();
-            if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+        try {
+            File file;
+            if (textEditor.currentFile == null) {
+                JFileChooser fileChooser = new JFileChooser();
+                if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION)
+                    return;
+
+                file = fileChooser.getSelectedFile();
+                textEditor.currentFile = file;
+            } else {
+                file = textEditor.currentFile;
             }
-        }
-        else{
-            //TODO
+
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(textArea.getText());
+            writer.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(textEditor, "Error reading file!");
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText("");
+        textEditor.currentFile = null;
     }
 }
